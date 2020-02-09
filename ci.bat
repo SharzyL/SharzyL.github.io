e71379210c9bb202347ef53a480754caab7d4df3
Sharzy <sharzyluo@outlook.com>
2020-02-09 10:47:20 +0800

    css modification

--- ci.bat
+++ ci.bat
@@ -1,1 +1,1 @@
-git add . & git commit -m %1 & git push & jekyll build & cd _site & git add . & git commit -m %1 & git push
+git add . & git commit -m %1 & git push & jekyll build & cd _site & git add . & git commit -m %1 & git push & cd ..
